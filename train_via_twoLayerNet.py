8812f438911b7b24c32acfc04021b4aa05839859
mhr380 <hajimemihara1108@gmail.com>
2017-01-01 14:47:35 +0900

    modified two_layer_net

--- train_via_twoLayerNet.py
+++ train_via_twoLayerNet.py
@@ -2,22 +2,25 @@
 
 import numpy as np
 from dataset.mnist import load_mnist
-import twoLayerNet
+from two_layer_net import TwoLayerNet
+import matplotlib.pyplot as plt
+
 
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
 
 train_loss_list = []
 
 # Hyper Params
-iters_num = 10000
+iters_num = 10
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
 
-network = twoLayerNet(intput_size=784, hidden_size=50, output_size=10)
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
 for i in range(iters_num):
-    batch_mask = np.random.choise(train_size, batch_size)
+    print('epoch: ' + str(i))
+    batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
 
@@ -28,3 +31,7 @@ for i in range(iters_num):
 
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
+
+
+plt.plot(np.array(train_loss_list))
+plt.show()
